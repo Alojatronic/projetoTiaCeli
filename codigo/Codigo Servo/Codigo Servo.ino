8e8f5d7697ec5cf9543cd7f4bb10b036631fe7dd
Louzeiro <flalouzeiro@hotmail.com>
2022-11-14 16:14:24 -0300

    Refinando o código de teste do servo moto

--- codigo/Codigo Servo/Codigo Servo.ino
+++ codigo/Codigo Servo/Codigo Servo.ino
@@ -1,31 +1,19 @@
 #include <Servo.h>
-#include <HCSR04.h>
-#include <Ultrasonic.h>
-#define TRIGGER   11
-#define ECHO      12
+
+// configurando a pinagem
+#define pin_servo 7
+
 Servo myservo; 
-Ultrasonic ultrasonic(TRIGGER, ECHO);
+Ultrasonic ultrasonic(pin_TRIGGER, pin_ECHO);
+
+
 void setup () {
-  //Inicializa a porta serial do Arduino com 9600 de baud rate
-    Serial.begin(9600);  
-    myservo.attach(7);  // Diz que o objeto "myservo" está ligado ao pino 9
+    myservo.attach(pin_servo);  
 }
 
 void loop () {
-  float distancia;
-  long microsec = ultrasonic.timing();
-  distancia = ultrasonic.convert(microsec, Ultrasonic::CM);
-  Serial.print("Distancia em cm: ");
-  Serial.println(distancia);
-        //if(distancia<=20){
-       // myservo.write(0); // Comando para mandar o servo para posição 180
-  //delay(500); // Espera de 500 ms
   myservo.write(180); // Comando para mandar o servo para posição 0
-  //delay(500); // Espera de 500 ms
-  myservo.write(45); // Comando para mandar o servo para posição 90
-  delay(1000);  // Espera de 500 ms
-       // }
-    
-    //Espera 1 segundo
-    //delay(1000);
+  delay(500);         // Espera de 500 ms
+  myservo.write(45);  // Comando para mandar o servo para posição 90
+  delay(1000);  // Espera de 1000 ms
 }
