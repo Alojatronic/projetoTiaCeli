06637ee126c19fed47973adf2a66cf63f063625a
Louzeiro <flalouzeiro@hotmail.com>
2022-11-14 16:24:53 -0300

    Refinando o código de teste do display LCD

--- codigo/Codigo Display 16 segmentos/Codigo Display 16 segmentos.ino
+++ codigo/Codigo Display 16 segmentos/Codigo Display 16 segmentos.ino
@@ -1,6 +1,15 @@
 #include <LiquidCrystal.h>;
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// Configurando a pinagem
+#define pin_RS 12
+#define pin_Enable 11
+#define pin_D4 5
+#define pin_D5 4
+#define pin_D6 3
+#define pin_D7 2
+
+// Configurando a biblioteca
+LiquidCrystal lcd(pin_RS, pin_Enable, pin_D4, pin_D5, pin_D6, pin_D7);
 
 void setup(){
   lcd.begin(16, 2);
@@ -14,7 +23,7 @@ void loop()
   lcd.setCursor(16,0);
   lcd.print("COLOQUE DINHEIROS HUMANO");
   lcd.setCursor(30,1);
-  lcd.print("PRECISAMOS");
+  lcd.print("PRECISAMOS! Assinado: Miau");
 
   for (int col = 0; col < 50; col++)
   {
